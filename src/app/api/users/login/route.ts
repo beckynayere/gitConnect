da7b76587bec5f39af0630ef11eb767c57a8f62e
beckynayere <rebecca.nayere@gmail.com>
2024-09-24 16:42:12 +0300

    create async function in the login route

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,6 +1,24 @@
 "use client";
-import axios from "axios";
-import Link from "next/link";
-import React, {useState} from "react";
-import {toast} from "react-hot-toast";
-import {useRouter} from "next/navigation";
+
+import {connect} from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
+
+
+connect()
+
+export async function POST(request: NextRequest){
+    try {
+        const reqBody = await request.json()
+        const {username, email, password} = reqBody
+
+        console.log(reqBody);
+
+        //check if user already exists
+        const user = await User.findOne({email})
+
+        if(user){
+            return NextResponse.json({error: "User already exists"}, {status: 400})
+        }
