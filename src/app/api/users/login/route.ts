f90cb44daeb6e2a78778604c5fca7b45e4574ef1
beckynayere <rebecca.nayere@gmail.com>
2024-09-24 22:06:14 +0300

    create logout page

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -4,8 +4,9 @@ import {connect} from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
-import { sendEmail } from "@/helpers/mailer";
+// import { sendEmail } from "@/helpers/mailer";
 import jwt from "jsonwebtoken";
+import { console } from "inspector";
 
 
 connect()
@@ -13,25 +14,26 @@ connect()
 export async function POST(request: NextRequest){
     try {
         const reqBody = await request.json()
-        const {username, email, password} = reqBody
+        const { email, password} = reqBody
 
         console.log(reqBody);
 
         //check if user already exists
         const user = await User.findOne({email})
 
-        if(user){
+        if(!user){
             return NextResponse.json({error: "User already exists"}, {status: 400})
         }
-
+        console.log("user exsists")
         // hash password
-        const salt = await bcryptjs.genSalt(10)
-        const hashedPassword = await bcryptjs.hash(password, salt)
+        // const salt = await bcryptjs.genSalt(10)
+        // const hashedPassword = await bcryptjs.hash(password, salt)
+
         const validPassword = await bcryptjs.compare (password, user.password)
         if(!validPassword){
             return NextResponse.json({ error: "Invalid Password"}, {status : 400})
         }
-
+        console.log(user );
         // token
         const tokenData = {
             id: user._id,
@@ -47,5 +49,10 @@ export async function POST(request: NextRequest){
         response.cookies.set("token", token,{
             httpOnly: true,
         })
-        return respnse;
+        return response;
+
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 500})
     }
+}
+    
