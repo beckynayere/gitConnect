0524a4c9b918122743d4de0d63d9d3db6ab537d8
beckynayere <rebecca.nayere@gmail.com>
2024-10-01 22:28:18 +0300

    config appwrite

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -27,7 +27,7 @@ export const sendEmail = async({email, emailType, userId}:any) => {
   });
 
         const mailOptions = {
-            from: 'fouty@gmail.com',
+            from: 'no.reply@beckymail.com',
             to: email,
             subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
             html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,52 +1,33 @@
-// import { verify } from "crypto";
-import mongoose from "mongoose";
-// import { type } from "os";
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
     username: {
-        type:String,
-        unique:true,
+        type: String,
+        unique: true,
         required: [true, "Please provide a username"],
     },
     email: {
-        type:String,
-        required: ["Please provide an email"],
+        type: String,
+        required: [true, "Please provide an email"],
         unique: true,
-        match: [/.+\@.+\..+/, "Please provide a valid email address"]
     },
-    password:{
-        type:String,
-        required: ["Please provide a password"]
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
     },
     isVerified: {
-        type:Boolean,
-        default:false,
+        type: Boolean,
+        default: false,
     },
     isAdmin: {
-        type:Boolean,
-        default:false,
+        type: Boolean,
+        default: false,
     },
-    
-    forgotPasswordToken: {
-        type: String,
-        default: null,
-    },
-    forgotPasswordTokenExpiry: {
-        type: Date,
-        default: null,
-    },
-    verifyToken: {
-        type: String,
-        default: null,
-    },
-    verifyTokenExpiry: {
-        type: Date,
-        default: null,
-    }
-}, {
-    timestamps: true,
+    forgotPasswordToken: { type: String },
+    forgotPasswordTokenExpiry: { type: Date },
+    verifyToken: { type: String }, // Make sure this is a string
+    verifyTokenExpiry: { type: Date },
 });
 
-const User = mongoose.models.User || mongoose.model("User", userSchema);
-
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 export default User;
